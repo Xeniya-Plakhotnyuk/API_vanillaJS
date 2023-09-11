6ebcab79d2d2a8ab947d1331c85e61ee5f4f0618
XeniyaZ <kseniya10081989@gmail.com>
2023-09-11 11:05:16 -0500

    complete first part

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 
-let img = document.querySelector('.img')
-document.querySelector('button').addEventListener("click", clicked)
+// let img = document.querySelector('.img')
+// document.querySelector('button').addEventListener("click", clicked)
 
-function clicked(){
-    console.log('click!!!')
-}
+// function clicked(){
+//     console.log('click!!!')
+// }
 
-fetch ("https://dog.ceo/api/breeds/image/random")
+// fetch ("https://dog.ceo/api/breeds/image/random")
+// .then(responce => responce.json())
+// .then(data => {
+//     img.innerHTML = `
+//     <img src='${data.message}' 
+//     width="300px" 
+//     alt = "dog"/>`})
+
+
+let phraze = document.querySelector('.advice')
+let btn = document.querySelector('button').addEventListener("click", getAdvice)
+
+
+function getAdvice(){
+  fetch('https://www.boredapi.com/api/activity')
 .then(responce => responce.json())
 .then(data => {
-    img.innerHTML = `
-    <img src='${data.message}' 
-    width="300px" 
-    alt = "dog"/>`})
+    console.log(data)
+    phraze.textContent = data.activity}
+)
+  }
+
+
+
+
 
