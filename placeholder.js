863d4249068cd487c82ba482abaa096183a515e0
XeniyaZ <kseniya10081989@gmail.com>
2023-09-14 13:58:45 -0500

    add placeholder app

--- placeholder.js
+++ placeholder.js
@@ -33,6 +33,14 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
     
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(post => {
+            document.querySelector(".request").innerHTML = `
+            <h3 class="blah">${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+            ${document.querySelector(".request").innerHTML}
+        `
 
+        })
 })
+
